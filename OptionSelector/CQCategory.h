0dbd83415527253ac55010f6065a91eb7a849642
ayunav <ayunanyc@gmail.com>
2015-08-28 15:17:11 -0400

    added description in comments for CQCategory class in CQCategory.h file. As per Caleb's suggestion.

--- OptionSelector/CQCategory.h
+++ OptionSelector/CQCategory.h
@@ -9,7 +9,12 @@
 #import <Foundation/Foundation.h>
 
 @interface CQCategory : NSObject
-
+/**
+ * This represents a category to be shown on the main screen. It stores the
+ * category's name, a list of the possible values under that category, and the
+ * currently selected item in the category. This data is initialized and used
+ * in MainTableViewController.
+ */
 @property (nonatomic) NSString *name;
 @property (nonatomic) NSArray *options;
 @property (nonatomic) NSString *selection;
